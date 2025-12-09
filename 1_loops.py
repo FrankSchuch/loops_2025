b54432b8d63f70de2fc037075fc89e54846e7075
FrankSchuch <187791840+FrankSchuch@users.noreply.github.com>
2025-12-09 20:42:36 +0000

    for, while loops

--- 1_loops.py
+++ 1_loops.py
@@ -1,27 +1,56 @@
 # Example Practice:
 # Given this list of fruits:
-fruits = ["apple", "banana", "cherry", "date"]
+# fruits = ["apple", "banana", "cherry", "date"]
+# print(len(fruits))
+# # Challenge:
+# # Use a for loop to print each fruit on a new line.
+# print(fruits[0])
+# print(fruits[1])
+# print(fruits[2])
+# for fruit in fruits:
+#     print(fruit)
+# # i just worked with loops
 
-# Challenge:
-# Use a for loop to print each fruit on a new line.
-print(fruits[0])
-print(fruits[1])
-print(fruits[2])
-for fruit in fruits:
-    print(fruit)
-# i just worked with loops
-
-# Given a list of school subjects:
+# # Given a list of school subjects:
 subjects = ["Math", "Science", "History", "Art"]
-for subject in subjects:
-    print(subject)
-# Challenge:
-# Use a for loop and range to print each subject along with its index:
-# Example output: "Subject 0: Math"
+# print(len(subjects))
+# for subject in subjects:
+#     print(subject)
+
+# for subject in subjects:
+#     print(subject)
+#     if subject == "History":
+#         break
+#     else:
+#         print(subject)
 
 
-# Given:
-numbers = [5, 10, 15, 20]
+# for subject in subjects:
+#     if subject == "Science":
+#         continue
+#     else: 
+#         print(subject)
+    # for index in range(len(subjects)):
+    #     print("Subject "+ str(index) + ": " + subjects[index])
 
-# Challenge:
-# Use a for loop to add all the numbers and print the total.
+    # numbers = [5, 10, 15, 20]
+    # total = 0
+    # for number in numbers:
+    #     total += number
+    # print("total: ", total)
+
+    # # Challenge:
+    # # Use a for loop and range to print each subject along with its index:
+    # # Example output: "Subject 0: Math"
+
+
+    # # Given:
+    # numbers = [5, 10, 15, 20]
+
+    # # Challenge:
+    # # Use a for loop to add all the numbers and print the total.
+    # new_total = 0
+    # new_numbers = list(range(1,600001))
+    # for number in new_numbers:
+    #     total += str(new_numbers)
+    # print("total: ", new_total)
